ced65baca96bc19635d8410459d907390ac50197
Killayt <surnamename615@gmail.com>
2023-04-24 12:44:24 +0300

    assa

---
+++ cmd/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"meetme/server"
+)
+
+func main() {
+	server.Run()
+}
---
+++ server/server.go
@@ -0,0 +1,24 @@
+package server
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func Run() {
+
+	gin.SetMode(gin.ReleaseMode)
+
+	r := gin.Default()
+	r.GET("/", indexHandler)
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	// r.LoadHTMLFiles("web/frontend")
+}
+
+func indexHandler(c *gin.Context) {
+	c.File("web/frontend/index.html")
+}
+
+// func indexHandler(c *gin.Context) {
+//   // Send a 200 status code and the message "Hello, World!" in the response body
+//   c.String(http.StatusOK, "Hello, World!")
+// }
